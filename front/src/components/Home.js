9ec58b78739908419f52bc6243fa42804b58fe44
blogginghindi@gmail.com <arshadnoor585@gmail.com>
2024-03-31 17:04:56 +0530

    fixed

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import LeftSidebar from "./LeftSidebar.js";
-import RightSidebar from "./RightSidebar.js";
+import LeftSidebar from "./LeftSidebar";
+import RightSidebar from "./RightSidebar";
 import { Outlet, useNavigate } from "react-router-dom";
-import useOtherUsers from "../hooks/useOtherUsers.js";
+import useOtherUsers from "../hooks/useOtherUsers";
 import { useSelector } from "react-redux";
-import useGetMyTweets from "../hooks/useGetMyTweets.js";
+import useGetMyTweets from "../hooks/useGetMyTweets";
 
 const Home = () => {
   const { user, otherUsers } = useSelector((store) => store.user);
--- front/src/index.js
+++ front/src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
-
-let persistor = persistStore(store);
+import store from "./redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
